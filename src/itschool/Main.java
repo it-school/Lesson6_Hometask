991b05b4d33021f1849e39b7dcdd3d550e3c79eb
Viktor Speranskyy <it@osvita.od.ua>
2019-10-10 19:00:35 +0300

    Minor enhancements

--- src/itschool/Main.java
+++ src/itschool/Main.java
@@ -7,7 +7,6 @@ public class Main {
     static String [] mark = {"BMW", "VAZ", "ZAZ", "GAZ", "LUAZ"};
 
     public static void main(String[] args) {
-	// write your code here
         Random random = new Random();
         Auto auto1 = new Auto(1234567890, "BYD");
         Auto auto2 = new Auto(2345678, "BMW", "silver", "asgdthsfydjukfig");
